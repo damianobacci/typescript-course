3d981588c3efdaaf665b43f19f2ccdb0deb2311e
Damiano Bacci <damiano.bacci@gmail.com>
2024-03-21 10:09:34 +0100

    Basics of TS course

---
+++ src/basics.ts
@@ -0,0 +1,12 @@
+function add(n1: number, n2: number, showResult: boolean) {
+  if (showResult) {
+    console.log(n1 + n2);
+  }
+  return n1 + n2;
+}
+
+const number1 = 4;
+const number2 = 2.8;
+const printResult = true;
+
+const result = add(number1, number2, printResult);
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+const person: {
+  name: string;
+  age: number;
+} = {
+  age: 30,
+  name: "Damiano",
+};
+
+console.log(person);
